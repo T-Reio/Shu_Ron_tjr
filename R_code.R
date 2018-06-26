bbc139b058e0d4446abf14b0bf6bf62fa3508193
T-Reio <ot.shunie.rt@gmail.com>
2018-06-26 16:17:32 +0900

    no message

--- R_code.R
+++ R_code.R
@@ -1,61 +1,58 @@
 library(data.table)
-tstats_npb<-fread("C:/Users/T-Reio/Master_thesis/team_stats_npb.csv",header=T,sep=",")
+tstats_npb<-fread("C:/users/T-Reio/Master_thesis/team_stats_npb.csv",header=T,sep=",")
 summary(tstats_npb)
-Avg<-tstats_npb$AVG
-Obp<-tstats_npb$OBP
+AVG<-tstats_npb$AVG
+OBP<-tstats_npb$OBP
 WA<-tstats_npb$WA
-Slg<-tstats_npb$SLG
-Runs<-tstats_npb$Runs
-ERuns<-tstats_npb$Eruns
+SLG<-tstats_npb$SLG
+R<-tstats_npb$Runs
+RA<-tstats_npb$Eruns
 
-plot(Avg,Runs)
-plot(Obp,Runs)
-plot(Slg,Runs)
-RegObp<-lm(Runs~Obp,data=tstats_npb)
-RegAvg<-lm(Runs~Avg,data=tstats_npb)
-RegSlg<-lm(Runs~Slg,data=tstats_npb)
-OPS=Obp+Slg
-RegOPS<-lm(Runs~OPS,data=tstats_npb)
+RegOBP<-lm(R~OBP,data=tstats_npb)
+RegAVG<-lm(R~AVG,data=tstats_npb)
+RegSLG<-lm(R~SLG,data=tstats_npb)
+OPS=OBP+SLG
+RegOPS<-lm(R~OPS,data=tstats_npb)
 
-WARegObp<-lm(WA~Obp,data=tstats_npb)
-WARegAvg<-lm(WA~Avg,data=tstats_npb)
-WARegSlg<-lm(WA~Slg,data=tstats_npb)
+WARegOBP<-lm(WA~OBP,data=tstats_npb)
+WARegAVG<-lm(WA~AVG,data=tstats_npb)
+WARegSLG<-lm(WA~SLG,data=tstats_npb)
 WARegOPS<-lm(WA~OPS,data=tstats_npb)
 
-WARegObp1<-lm(WA~Obp+ERuns,data=tstats_npb)
-WARegAvg1<-lm(WA~Avg+ERuns,data=tstats_npb)
-WARegSlg1<-lm(WA~Slg+ERuns,data=tstats_npb)
-WARegOPS1<-lm(WA~OPS+ERuns,data=tstats_npb)
+WARegOBP1<-lm(WA~OBP+RA,data=tstats_npb)
+WARegAVG1<-lm(WA~AVG+RA,data=tstats_npb)
+WARegSLG1<-lm(WA~SLG+RA,data=tstats_npb)
+WARegOPS1<-lm(WA~OPS+RA,data=tstats_npb)
 
-SRRegOPS<-lm(Runs~Obp+Slg,data=tstats_npb)
-SWARegOPS<-lm(WA~Obp+Slg+ERuns,data=tstats_npb)
+SRRegOPS<-lm(R~OBP+SLG,data=tstats_npb)
+SWARegOPS<-lm(WA~OBP+SLG+RA,data=tstats_npb)
 
-SRRegAPS<-lm(Runs~Avg+Slg,data=tstats_npb)
-SWARegAPS<-lm(WA~Avg+Slg+ERuns,data=tstats_npb)
+SRRegAPS<-lm(R~AVG+SLG,data=tstats_npb)
+SWARegAPS<-lm(WA~AVG+SLG+RA,data=tstats_npb)
 
 
-cor(Avg,Runs)
-cor(Obp,Runs)
-cor(Slg,Runs)
-cor(OPS,Runs)
-cor(Avg,WA)
-cor(Obp,WA)
-cor(Slg,WA)
+cor(AVG,R)
+cor(OBP,R)
+cor(SLG,R)
+cor(OPS,R)
+cor(AVG,WA)
+cor(OBP,WA)
+cor(SLG,WA)
 cor(OPS,WA)
 
-summary(RegAvg)
-summary(RegSlg)
-summary(RegObp)
+summary(RegAVG)
+summary(RegSLG)
+summary(RegOBP)
 summary(RegOPS)
 
-summary(WARegAvg)
-summary(WARegSlg)
-summary(WARegObp)
+summary(WARegAVG)
+summary(WARegSLG)
+summary(WARegOBP)
 summary(WARegOPS)
 
-summary(WARegAvg1)
-summary(WARegSlg1)
-summary(WARegObp1)
+summary(WARegAVG1)
+summary(WARegSLG1)
+summary(WARegOBP1)
 summary(WARegOPS1)
 
 summary(SRRegOPS)
@@ -64,7 +61,8 @@ summary(SWARegOPS)
 summary(SRRegAPS)
 summary(SWARegAPS)
 
-mean(Slg)
-median(Slg)
-mean(Obp)
-median(Obp)
+mean(SLG)
+median(SLG)
+mean(OBP)
+median(OBP)
+
